66e61214f9bb533b54fc2aecf267a9182101eea4
Omran Alrbedan <om.alrbedan100@gmail.com>
2025-09-18 10:06:54 -0700

    fix bugs

--- app/(root)/[locale]/profile/page.tsx
+++ app/(root)/[locale]/profile/page.tsx
@@ -1,31 +1,7 @@
-import dynamic from "next/dynamic";
-import { Suspense } from "react";
-
-// استيراد المكون باستخدام Dynamic Import مع تعطيل SSR
-const ClientProfileContent = dynamic(
-  () => import("./_components/ClientProfileContent"),
-  {
-    ssr: false,
-    loading: () => (
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-blue-500"></div>
-      </div>
-    ),
-  }
-);
+import ProfileClientWrapper from "./ProfileClientWrapper";
 
 const ProfilePage = () => {
-  return (
-    <Suspense
-      fallback={
-        <div className="min-h-screen flex items-center justify-center">
-          <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-blue-500"></div>
-        </div>
-      }
-    >
-      <ClientProfileContent />
-    </Suspense>
-  );
+  return <ProfileClientWrapper />;
 };
 
 export default ProfilePage;
