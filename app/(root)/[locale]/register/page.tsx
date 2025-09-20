809e8ecbd695123ea1a6f8259c18b4fe88b3d98c
Omran Alrbedan <om.alrbedan100@gmail.com>
2025-09-20 09:47:13 -0700

    try

---
+++ app/(root)/[locale]/register/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { images } from "@/constants/images";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import dynamic from "next/dynamic";
+const RegisterForm = dynamic(() => import("@/components/forms/RegisterForm"), {
+  ssr: false,
+});
+
+export default function RegisterPage() {
+  return (
+    <div className="flex mx-auto w-fit lg:grid-cols-2 min-h-[80vh] mt-20">
+      {/* Left Column - Form Section */}
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <motion.div
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.1 }}
+          className="flex flex-1 items-center justify-center"
+        >
+          <div className="w-full max-w-2xl">
+            <RegisterForm />
+          </div>
+        </motion.div>
+      </div>
+
+      {/* Right Column - Image Section */}
+      <div className="relative items-center hidden bg-muted md:flex">
+        <motion.div
+          initial={{ x: 120, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.3 }}
+          className="w-full h-full flex items-center justify-center"
+        >
+          <Image
+            src={images.signUp}
+            alt="Register illustration"
+            height={480}
+            width={480}
+            className="relative"
+            priority
+          />
+        </motion.div>
+      </div>
+    </div>
+  );
+}
